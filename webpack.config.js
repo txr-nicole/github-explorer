5538ee5374d15c235b9b57024cae6679a9079d82
txr-nicole <nicolefagundes24@gmail.com>
2023-03-29 16:08:10 -0300

    🔧 add reack refresh plugin to development

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 const isDevelopment = process.env.NODE_ENV != "production";
 
@@ -18,17 +19,25 @@ module.exports = {
     static: path.resolve(__dirname, "public"),
   },
   plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
     }),
-  ],
+  ].filter(Boolean),
   module: {
     // module vai ser como a nossa aplicação vai se comportar quando tiver importando diferentes tipos de arquivos
     rules: [
       {
         test: /\.jsx$/, // todos arquivos jsx | $ = arquivos com final assim
         exclude: /node_modules/, // menos os da pasta node_modules
-        use: "babel-loader",
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              isDevelopment && require.resolve("react-refresh/babel"),
+            ].filter(Boolean),
+          },
+        },
       },
       {
         test: /\.scss$/, // todos arquivos css | $ = arquivos com final assim
