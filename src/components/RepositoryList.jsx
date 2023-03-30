7fef8a83a2b7f3b57c073970e1dfc21939a7503a
txr-nicole <nicolefagundes24@gmail.com>
2023-03-30 13:35:45 -0300

    💄 add repository list

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,24 @@
+import { RepositoryItem } from "./RepositoryItem";
+import "..styles/repositories.scss"
+
+const repository = {
+  name:"unform",
+  description:"Forms in React",
+  link:"https://twitch.tv/felps"
+}
+
+const repositoryName = "unform45";
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+      <ul>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+      </ul>
+    </section>
+  );
+}
