724eebecfa80eac918739ce13ce026e119c75a09
txr-nicole <nicolefagundes24@gmail.com>
2023-03-31 15:26:22 -0300

    ✨ make the repository list dynamic

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -3,7 +3,7 @@ export function RepositoryItem(props) {
     <li>
       <strong>{props.repository.name ?? "Não temos"}</strong>
       <p>{props.repository.description}</p>
-      <a href={props.repository.link}>Acessar Repositório</a>
+      <a href={props.repository.html_url}>Acessar Repositório</a>
     </li>
   );
 }
